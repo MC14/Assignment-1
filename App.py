27deabcd46e3eac2b1794fd031b121c843c41e48
MC14 <morgan.cowans@outlook.com>
2022-10-02 14:51:34 -0400

    Challenge 1 test

--- App.py
+++ App.py
@@ -2,14 +2,28 @@
 #The game will give a short description of the game as well as import the roles and game systems for then user to play
 #
 import random
+from unittest import result
+import Game
 
 print("Hello Player, Welcome to the heist") #Introduces Player to the game
 Char_Choice = input("Please pick a Character 1.Thug 2. Spy: ")
 if Char_Choice == "Thug":
     import Role1
+    Game.stat_phy = Role1.t_Phy
+    Game.stat_IQ = Role1.t_IQ
+    Game.stat_CHA = Role1.t_CHA
+    Game.stat_HP = Role1.t_HP
     print("You chose the brutal Thug")
 if Char_Choice == "Spy":
     import Role2
     print("You chose the sneaky Spy")
+
+print("You job will be to break into the Forfur Manor and steal the Gem that rest in the display. you'll have to get pass security, get through the guests, sneak into the room and get the gem.")
+
+Chal_1 = input("First you'll have to getb past the security of the party. are you [charm] them or [knock] him out?:")
+if Chal_1 == "knock":
+    Game.roll.phy(result)
+    print(result)
+
 
 
--- Role1.py
+++ Role1.py
@@ -5,20 +5,8 @@ import random
 
 t_Phy = 2 # The Physical stat for the Thug, will be used in challenges
 t_IQ = 0 #The Intelligence stat for the Thug, will be used in challenges
-t_CHA = -2 #The Charisma Stat for the Thug, will be used in challenges
+t_CHA = -1 #The Charisma Stat for the Thug, will be used in challenges
 t_HP = 3 #The Health Stat for the Thug, the amount of redo's the Thug has before getting a game over
 
-def roll_Phy(result):
- dice_roll = random.randrange (1, 13)
- result = dice_roll + t_Phy
- return result
 
-def roll_IQ(result):
- dice_roll = random.randrange (1, 13)
- result = dice_roll + t_IQ
- return result
 
-def roll_CHA(result):
- dice_roll = random.randrange (1, 13)
- result = dice_roll + t_CHA
- return result
