73121a816b95580bd1a7c54ea5104feb56ec603b
MC14 <morgan.cowans@outlook.com>
2022-10-04 21:42:39 -0400

    Taking out HP

--- Role1.py
+++ Role1.py
@@ -2,7 +2,7 @@
 t_Phy = 2 # The Physical stat for the Thug, will be used in challenges
 t_IQ = 0 #The Intelligence stat for the Thug, will be used in challenges
 t_CHA = -1 #The Charisma Stat for the Thug, will be used in challenges
-t_HP = 3 #The Health Stat for the Thug, the amount of redo's the Thug has before getting a game over
+
 
 
 
--- Role2.py
+++ Role2.py
@@ -2,4 +2,3 @@
 s_Phy = 0 # The Physical stat for the Spy, will be used in challenges
 s_IQ = 1 #The Intelligence stat for the Spy, will be used in challenges
 s_CHA = 2 #The Charisma Stat for the Spy, will be used in challenges
-s_HP = 2 #The Health Stat for the Spy, the amount of redo's the Spy has before getting a game over
