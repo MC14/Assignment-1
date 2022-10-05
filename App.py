dc35ad602c4dbf4e38318e7e5eb2c11193c66b8f
MC14 <morgan.cowans@outlook.com>
2022-10-04 21:51:27 -0400

    testing out time.sleep

--- App.py
+++ App.py
@@ -6,7 +6,9 @@ import random
 import Game
 import Role1
 import Role2
-print("Hello Player, Welcome to the heist") #Introduces Player to the game
+import time 
+print("Hello Player, Welcome to the Forfur manor heist: Press 'enter' to continue. ") #Introduces Player to the game
+time.sleep()
 Char_Choice = input("Please pick a Character 1.Thug 2. Spy: ")
 if Char_Choice == "Thug":
     
@@ -20,23 +22,21 @@ elif Char_Choice == "Spy":
     print("You chose the sneaky Spy")
 else:
     print ("not a valid option")
-
-
 print("You job will be to break into the Forfur Manor and steal the Gem that rest in the display. you'll have to get pass security, get through the guests, get into the room and get the gem.")
 
-Chal_1 = input("First you'll have to get past the security of the party. are you [charm] them or [knock] him out?:")
+Chal_1 = input("First you'll have to get past the security of the party. are you [charm] them or [knock] him out?: ")
 if Chal_1 == "knock":
     Roll = Game.roll_phy(Game.stat_phy)
 if Chal_1 == "charm":
     Roll = Game.roll_CHA(Game.stat_CHA)
 
-Chal_2 = input("The party is in full swing, but now you have to slip past, do you plot a [route] to slip by undetected or [blend] in with the guest and wait for your time to strike")
+Chal_2 = input("The party is in full swing, but now you have to slip past, do you plot a [route] to slip by undetected or [blend] in with the guest and wait for your time to strike: ")
 if Chal_2 == "route":
     Roll = Game.roll_IQ(Game.stat_IQ)
 if Chal_2 == "blend":
     Roll = Game.roll_CHA(Game.stat_CHA)
 
-Chal_3 = input("you found the room where the Gem is being kept, but it's locked. do you [break] the door down or [pick] the lock")
+Chal_3 = input("you found the room where the Gem is being kept, but it's locked. do you [break] the door down or [pick] the lock: ")
 if Chal_3 == "break":
     Roll = Game.roll_phy(Game.stat_phy)
 if Chal_3 == "pick":
