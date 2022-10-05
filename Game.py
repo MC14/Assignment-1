81f17c4e3407b43683cdf2f9de4f4c5f4ce03117
MC14 <morgan.cowans@outlook.com>
2022-10-04 21:42:45 -0400

    Taking out HP

--- Game.py
+++ Game.py
@@ -7,7 +7,6 @@ import Role2 #allows you to use the Role 2 stats
 stat_phy = 0 #variables for the stats in order to put the Role stats into for the functions
 stat_IQ = 0
 stat_CHA = 0
-stat_HP = 0
 
 def roll_phy(stat_phy, stat_HP):
 
@@ -18,17 +17,12 @@ def roll_phy(stat_phy, stat_HP):
     print(dice_roll1,"+",dice_roll2,"+",stat_phy,"=",dice_result)
     if dice_result <= 3:
         print("Mega Failure!")
-        stat_phy = stat_phy - 1
-        stat_HP = stat_HP - 1
-        if stat_HP == 0:
-            print("Game Over, you ran out of HP")
-            exit()
+        print("Game over!, you only get once chance to steal the Gem")
+        exit()
     if dice_result > 3 and dice_result <= 7:
         print("Failure")
-        stat_HP = stat_HP - 1
-        if stat_HP == 0:
-            print("Game Over, you ran out of HP")
-            exit()
+        print("Game over!, you only get once chance to steal the Gem")
+        exit()
     if dice_result > 7 and dice_result <=10:
         print("Success, you made it")
     if dice_result > 10:
@@ -47,17 +41,12 @@ def roll_IQ(stat_IQ, stat_HP):
     print(dice_roll1,"+",dice_roll2,"+",stat_IQ,"=",dice_result)
     if dice_result <= 3:
         print("Mega Failure!")
-        stat_IQ = stat_IQ - 1
-        stat_HP = stat_HP - 1
-        if stat_HP == 0:
-            print("Game Over, you ran out of HP")
-            exit()
+        print("Game over!, you only get once chance to steal the Gem")
+        exit()
     if dice_result > 3 and dice_result <= 7:
         print("Failure")
-        stat_HP = stat_HP - 1
-        if stat_HP == 0:
-            print("Game Over, you ran out of HP")
-            exit()
+        print("Game over!, you only get once chance to steal the Gem")
+        exit()
     if dice_result > 7 and dice_result <=10:
         print("Success, you made it")
     if dice_result > 10:
@@ -73,20 +62,10 @@ def roll_CHA(stat_CHA, stat_HP):
     print(dice_roll1,"+",dice_roll2,"+",stat_CHA,"=",dice_result)
     if dice_result <= 3:
         print("Mega Failure!")
-        stat_CHA = stat_CHA - 1
-        stat_HP = stat_HP - 1
-        print(stat_HP)
-        print(stat_CHA)
-        if stat_HP == 0:
-            print("Game Over, you ran out of HP")
-            exit()
+        print("Game over!, you only get once chance to steal the Gem")
     if dice_result > 3 and dice_result <= 7:
         print("Failure")
-        print(stat_HP)
-        stat_HP = stat_HP - 1
-        if stat_HP == 0:
-            print("Game Over, you ran out of HP")
-            exit()
+        print("Game over!, you only get once chance to steal the Gem")
     if dice_result > 7 and dice_result <=10:
         print("Success, you made it")
     if dice_result > 10:
