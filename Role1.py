d4bb5ad039fd955951b4c882ce8a14a94b381507
MC14 <morgan.cowans@outlook.com>
2022-10-11 13:02:00 -0400

    l

--- Role1.py
+++ Role1.py
@@ -1,7 +1,7 @@
 
 class Thug:
  def __init__(self):
-    self.r_Phy = 2 # The Physical stat for the Thug, will be used in challenges
+    self.r_phy = 2 # The Physical stat for the Thug, will be used in challenges
     self.r_IQ = 0 #The Intelligence stat for the Thug, will be used in challenges
     self.r_CHA = -1 #The Charisma Stat for the Thug, will be used in challenges
 
