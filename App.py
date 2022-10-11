673a6e0e53c19d3f3f74415730d27520af44e82d
MC14 <morgan.cowans@outlook.com>
2022-10-11 19:47:40 -0400

    Commenting + little changes

--- App.py
+++ App.py
@@ -6,79 +6,107 @@ import time # the time module, I imported it to use the sleep function, this wil
 
 
 print("Hello Player, Welcome to the Forfur Manor heist.") #Introduces Player to the game
+
 time.sleep(4) #gives a break in the message in order form the player to read it easier
+
 print("You job will be to break into the Forfur Manor and steal the Gem that rest in the display. you'll have to get pass security, get through the guests, get into the room and get the gem.") #explaining the backstory to the game
+
 print("in order to pick what to do, please type the word that with be in the square brackets like this [example] exactly as written.") #explains how to use the input command and what to type in in order to reduce confusion
+
 time.sleep(6)
-while Game.loop_trigger == 0 :
+while Game.loop_trigger == 0 :#a simple while loop using a variable from the program that makes sure you stay in the loop until loop becomes 1
+    
     Char_Choice = input("Please pick a Character 1.[Thug] 2. [Spy]: ") #gives the input command to choose what type of role you wanna play
+    
     if Char_Choice == "Thug": #an if statement saying that if they type Thug, it will make the Game stats used for rolls the same value as the role pick, either Thug or Spy
-        import Role1
-        role_d = Role1.Thug() #adds a variable
+        import Role1 #import info from Role 1 program
+        role_d = Role1.Thug() #adds a refrence variable to the class Thug in order to be able to pull variables from it 
         print("You chose the brutal Thug") #Prints message to tell player they're choice was recieved
-        Game.loop_trigger = 1
-    elif Char_Choice == "Spy": #an elif (else-if) statement so if Spy is chosen instead of Thug,
-        import Role2
-        role_d = Role2.Spy()
+        Game.loop_trigger = 1 #make the loop variable 1 in order to break the loop and continue the game in the next loop/ 
+    
+    elif Char_Choice == "Spy": #an elif (else-if) statement so if Spy is chosen instead of Thug, it grabs the stuff from the Spy class in role 2
+        import Role2 #import info from Role 2 program
+        role_d = Role2.Spy() ##adds a refrence variable to the class Spy in order to be able to pull variables from it
         print("You chose the sneaky Spy") #Prints a message to tell player they're choice was recieved
         Game.loop_trigger = 1
+    
     else: #else means any other input/message besides the ones describe fall under here
         print("not a valid option") #tells the player the message wasn't correct
         continue# continues the loop and keeps it going
 
 
-Game.loop_trigger = 0
-while Game.loop_trigger == 0 :
-    Chal_1 = input("First you'll have to get past the security of the party.are you gonna [charm] him to let them pass or [knock] him out?: ")
-    if Chal_1 == "knock":
-        Roll = Game.roll_phy(role_d)
+Game.loop_trigger = 0 #makes loop 0 again in order to make another loop for the input decision
+while Game.loop_trigger == 0 : #this while loop is for first choice of the game, rather "charm" or "knock"
+    
+    Chal_1 = input("First you'll have to get past the security of the party.are you gonna [charm] him to let them pass or [knock] him out?: ") #input command with a set-up for story reasons
+    
+    if Chal_1 == "knock": #if statement for typing "knock"
+        Roll = Game.roll_phy(role_d) #Calls Game function roll_phy using role_d, this rolls the dice and then continues once done
+        Game.loop_trigger = 1 #once again loop = 1 to break the loop and continue the game
+    
+    elif Chal_1 == "charm": #elif statement for the choice "charm"
+        Roll = Game.roll_CHA(role_d) #rolls the Cha version for the roll function
         Game.loop_trigger = 1
-    elif Chal_1 == "charm":
-        Roll = Game.roll_CHA(role_d)
-        Game.loop_trigger = 1
+    
     else:
-         print ("not a valid option")
+         print ("not a valid option") #prints this and then continues the loop to try again
 
 Game.loop_trigger = 0
-while Game.loop_trigger == 0:
+while Game.loop_trigger == 0: #while loop for next challenge, this continues for the next couple of while loops with different combinations of stats to test your luck
+    
     Chal_2 = input("The party is in full swing, but now you have to slip past, do you plot the best [route] to slip by undetected or [blend] in with the guest and wait for your time to strike: ")
+    
     if Chal_2 == "route":
-        Roll = Game.roll_IQ(role_d)
+        Roll = Game.roll_IQ(role_d) #this variable calls the IQ version of the Roll function within the Game program
         Game.loop_trigger = 1
+    
     elif Chal_2 == "blend":
         Roll = Game.roll_CHA(role_d)
         Game.loop_trigger = 1
+    
     else:
         print ("not a valid option")
 Game.loop_trigger = 0
 
 while Game.loop_trigger == 0:
+    
     Chal_3 = input("you found the room where the Gem is being kept, but it's locked. do you [break] the door down or [pick] the lock?: ")
+    
     if Chal_3 == "break":
         Roll = Game.roll_phy(role_d)
         Game.loop_trigger = 1
+    
     elif Chal_3 == "pick":
         Roll = Game.roll_IQ(role_d)
         Game.loop_trigger = 1
+    
     else:
         print ("not a valid option")
 Game.loop_trigger = 0
+
 print("You're finally into the room. the Gem is seating inside a glass case, there's a key pass and multiple cameras watching the gem.")
+
 time.sleep(2)
-while Game.loop_trigger == 0:
+
+while Game.loop_trigger == 0: #In this loop, it gives three potential answer, one for each stat to allow you to use whatever stat is best to finish the job
+    
     Chal_4 = input("do you [shatter] the glass case, [decrypt] the key code or [disguise] yourself to fool the cameras?: ")
+    
     if Chal_4 == "shatter":
         Roll = Game.roll_phy(role_d)
         Game.loop_trigger = 1
+    
     elif Chal_4 == "decrypt":
         Roll = Game.roll_IQ(role_d)
         Game.loop_trigger = 1
+    
     elif Chal_4 == "disguise":
         Roll = Game.roll_CHA(role_d)
         Game.loop_trigger = 1
+    
     else:
         print ("not a valid option")
 
 print("congrats, you slipped away with the Gem in your hands, you're gonna be flithy rich!")
 time.sleep(4)
-exit()
+exit() #closes the program, stops it 
--- Game.py
+++ Game.py
@@ -1,40 +1,43 @@
+#This is the Game Program, this is where all of the Game's functions and work. it has three functions for each stat you can roll in the game
+#It uses the random module in order to gives us random numbers from a range to simulate dice and give a result that, if high enough passes the test and continue the game
+#but if too low, it gives a game over and ends the program
+
 
 import random #imports andom in order to be used in the program
 import Role1 #allows you to use the Role 1 stats
 import Role2 #allows you to use the Role 2 stats
 
 
-loop_trigger = 0
+loop_trigger = 0 #starts loop at zero so it can be used in App program
 
 
-def roll_phy(role_d):
+def roll_phy(role_d): #a function that takes the class from whatever role given in order to get the stats needed 
 
-    dice_roll1 = random.randrange (1,7)
-    dice_roll2 = random.randrange (1,7)
-    dice_result = dice_roll1 +dice_roll2 + role_d.r_phy
+    dice_roll1 = random.randrange (1,7) #a variable that uses randrange in order to give a random number from 1 to 6
+    dice_roll2 = random.randrange (1,7) # a second variable with the same purpose, but used for the print statement
+    dice_result = dice_roll1 +dice_roll2 + role_d.r_phy #variable gives the total from the two dice_roll variables and adds the phy stat from the role 
     
-    print(dice_roll1,"+",dice_roll2,"+",role_d.r_phy,"=",dice_result)
-    if dice_result <= 3:
+    print(dice_roll1,"+",dice_roll2,"+",role_d.r_phy,"=",dice_result) #print the outline of all the variables to show the player how it exactly added up
+    if dice_result <= 3: #start of the if statement to show what the exact result is, if less than 3, it games over and stops the game
         print("Mega Failure!")
-        print("Game over!, you only get once chance to steal the Gem")
-        exit()
+        print("Game over!, you only get once chance to steal the Gem") #tells the player they lost
+        exit() #exits the program
 
-    if dice_result > 3 and dice_result <= 6:
+    elif dice_result > 3 and dice_result <= 6: #if the result number is greater than 3 but less than or equal to 6, give this print statement
         print("Failure")
-        print("So close! but....Game over, you only get once chance to steal the Gem")
+        print("So close! but....Game over, you only get once chance to steal the Gem") #tells the player they lost
         exit()
 
-    if dice_result > 6 and dice_result <=10:
+    elif dice_result > 6 and dice_result <=10: #greater than 7 and less than or equal to 10, succeed and continue the game
         print("Success, you made it")
-    if dice_result > 10:
+    elif dice_result > 10: #if the result is greater than 10
         print("Super Success! You're really strong") 
-        role_d.r_phy = role_d.r_phy + 1
-        print(role_d.r_phy)
+        role_d.r_phy = role_d.r_phy + 1 #gives a little bonus for having a really good score, it ups the stat by 1, making it easier to get better scores
     
 
-    return dice_result
+    return dice_result #returns the dice_result and goes back to the App when done
 
-def roll_IQ(role_d):
+def roll_IQ(role_d): #follows many of the same code as roll_Phy but using the IQ stat instead of the Class Phy stat
     dice_roll1 = random.randrange (1,7)
     dice_roll2 = random.randrange (1,7)
     dice_result = dice_roll1 +dice_roll2 + role_d.r_IQ
@@ -44,19 +47,19 @@ def roll_IQ(role_d):
         print("Mega Failure!")
         print("Game over!, you only get once chance to steal the Gem")
         exit()
-    if dice_result > 3 and dice_result <= 6:
+    elif dice_result > 3 and dice_result <= 6:
         print("Failure")
         print("Game over!, you only get once chance to steal the Gem")
         exit()
-    if dice_result > 6 and dice_result <=10:
+    elif dice_result > 6 and dice_result <=10:
         print("Success, you made it")
-    if dice_result > 10:
+    elif dice_result > 10:
         print("Super Success!") 
         role_d.r_IQ = role_d.r_IQ + 1
         
     return dice_result
 
-def roll_CHA(role_d):
+def roll_CHA(role_d): #follows many of the same code as the other two but instead for the Class CHA stat
     dice_roll1 = random.randrange (1,7)
     dice_roll2 = random.randrange (1,7)
     dice_result = dice_roll1 +dice_roll2 + role_d.r_CHA
@@ -66,13 +69,13 @@ def roll_CHA(role_d):
         print("Mega Failure!")
         print("Game over!, you only get once chance to steal the Gem")
         exit()
-    if dice_result > 3 and dice_result <= 6:
+    elif dice_result > 3 and dice_result <= 6:
         print("Failure")
         print("Game over!, you only get once chance to steal the Gem")
         exit()
-    if dice_result > 6 and dice_result <=10:
+    elif dice_result > 6 and dice_result <=10:
         print("Success, you made it")
-    if dice_result > 10:
+    elif dice_result > 10:
         print("Super Success!") 
         role_d.r_CHA = role_d.r_CHA + 1
         print(role_d.r_CHA)
