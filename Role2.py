e1b244baa0c18f572ca64a9786a932b66bbd0d5b
MC14 <morgan.cowans@outlook.com>
2022-10-11 20:10:09 -0400

    commenting

--- Role2.py
+++ Role2.py
@@ -1,4 +1,4 @@
-# this is role 2, a program
+# this is role 2, a program that holds a class for the stats for the second role Spy
 class Spy: #class ID as Spy
  def __init__(self): #an initiated function that is executed when called, self is used to access variables within the class
     self.r_phy = 0 # The Physical stat for the Spy, will be used in challenges
