79a4aeccc790944e0e63b444af11a1153b88e2f0
MC14 <morgan.cowans@outlook.com>
2022-10-23 17:28:47 -0400

    change to assignment 2

--- Role2.py
+++
@@ -1,6 +0,0 @@
-# this is role 2, a program
-class Spy: #class ID as Spy
- def __init__(self): #an initiated function that is executed when called, self is used to access variables within the class
-    self.r_phy = 0 # The Physical stat for the Spy, will be used in challenges
-    self.r_IQ = 1 #The Intelligence stat for the Spy, will be used in challenges
-    self.r_CHA = 2 #The Charisma Stat for the Spy, will be used in challenges
