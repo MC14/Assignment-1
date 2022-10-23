650a6d79710bbd3d970fd5cdec73a3c4f3bbc8f8
MC14 <morgan.cowans@outlook.com>
2022-10-23 18:50:24 -0400

    start class Application + Input variables

--- Assignment-2/Assignment 2.py
+++ Assignment-2/Assignment 2.py
@@ -1,6 +1,11 @@
 class Application:
 
     print("Welcome to Morgan's Magnificent Products")
-    code = input()
-    def _init_(self):
+    code = input("Please enter your Product's code?: ")
+    name = input("what is the name of your Product?: ")
+    price = input("how much is the sale price of your Product?: ")
+    manu_cost = input("how much does it cost to build?: ")
+    Stock_lvl = input("What's your stock level?: ")
+    month_units_manu= input("What's the estimated amount of product made in a Month?: ")
+
 
