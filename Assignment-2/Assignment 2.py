70db648147f595bf6475c0c24a7ee293c7706424
MC14 <morgan.cowans@outlook.com>
2022-10-28 17:07:43 -0400

    print functions for variables

--- Assignment-2/Assignment 2.py
+++ Assignment-2/Assignment 2.py
@@ -1,22 +1,28 @@
 class Product:
     print("*** Morgan's Sample Stock Statment ***")
-    def _init_(self,code,name,price,manu_cost,stock_lvl,month_units_manu):
-        print("Product Code: ",code,)
+    def __init__(self,code,name,price,manu_cost,stock,month_units):
+        print("Product Code:",code,)
+        print("Product Name:",name,)
+
+        print("Sale Price:",price,"CAD")
+        print("Manufacture Cost:",manu_cost,"CAD")
+        print("Monthly Production:",month_units,"units (Approx.)")
+    
+    
+
 
 
 class Application:
 
     print("Welcome to Morgan's Magnificent Products")
-    code = input("Please enter your Product's code?: ")
-    name = input("what is the name of your Product?: ")
-    price = input("how much is the sale price of your Product?: ")
-    manu_cost = input("how much does it cost to build?: ")
-    stock_lvl = input("What's your stock level?: ")
-    month_units_manu= input("What's the estimated amount of product made in a Month?: ")
-    App_product = Product(code,name,price,manu_cost,stock_lvl,month_units_manu)
+    code = input("Please enter your Product's code: ")
+    name = input("Please enter the Name of your Product: ")
+    price = input("Please enter the Price of the Product: ")
+    manu_cost = input("Please enter the Manufacturing Cost: ")
+    stock = input("Please enter the amount of Stock of the Product: ")
+    month_units = input("Please enter the amount of stock made in a month: ")
 
 
 
-        
-
-
+    
+    App_product = Product(code,name,price,manu_cost,stock,month_units)
