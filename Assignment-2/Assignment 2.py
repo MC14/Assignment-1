7ed0eb82e7f1bde7b318902de86350fb8b1c40df
MC14 <morgan.cowans@outlook.com>
2022-10-28 15:47:00 -0400

    Testing out product/ print input statement

--- Assignment-2/Assignment 2.py
+++ Assignment-2/Assignment 2.py
@@ -5,7 +5,18 @@ class Application:
     name = input("what is the name of your Product?: ")
     price = input("how much is the sale price of your Product?: ")
     manu_cost = input("how much does it cost to build?: ")
-    Stock_lvl = input("What's your stock level?: ")
+    stock_lvl = input("What's your stock level?: ")
     month_units_manu= input("What's the estimated amount of product made in a Month?: ")
+    App_product = Product(code,name,price,manu_cost,stock_lvl,month_units_manu)
+
+
+
+
+
+class Product:
+    print("*** Morgan's Sample Stock Statment ***")
+    def _init_(self,code,name,price,manu_cost,stock_lvl,month_units_manu):
+        print("Product Code: ",code,)
+        
 
 
