5a691865e7f91c65bbad5a9477d55c858a456bf3
MC14 <morgan.cowans@outlook.com>
2022-10-28 18:25:15 -0400

    created while loops for each input to make sure that proper stuff is being put in

--- Assignment-2/Assignment 2.py
+++ Assignment-2/Assignment 2.py
@@ -1,6 +1,6 @@
 class Product:
-    print("*** Morgan's Sample Stock Statment ***")
     def __init__(self,code,name,price,manu_cost,stock,month_units):
+        print("*** Morgan's Sample Stock Statment ***")
         print("Product Code:",code,)
         print("Product Name:",name,)
 
@@ -14,15 +14,46 @@ class Product:
 
 class Application:
 
+    loop = False
     print("Welcome to Morgan's Magnificent Products")
-    code = input("Please enter your Product's code: ")
+    while loop == False:
+        code = input("Please enter your Product's code: ")
+        int(code)
+        if code >= 100 & code <= 1000:
+            loop == True
+        else:
+            print("Invalid Product Code, please try again")
+    loop = False
     name = input("Please enter the Name of your Product: ")
-    price = input("Please enter the Price of the Product: ")
-    manu_cost = input("Please enter the Manufacturing Cost: ")
-    stock = input("Please enter the amount of Stock of the Product: ")
-    month_units = input("Please enter the amount of stock made in a month: ")
-
-
-
-    
+    while loop == False:
+        price = input("Please enter the Price of the Product: ")
+        float(price)
+        if type(price) == float & price >= 0:
+            loop = True
+        else:
+            print("Invalid Price, please try again")
+    loop = False
+    while loop == False:
+        manu_cost = input("Please enter the Manufacturing Cost: ")
+        float(manu_cost)
+        if type(manu_cost) == float & manu_cost >= 0:
+            loop = True
+        else:
+            print("Invalid Manufacture Cost, please try again")
+    loop = False
+    while loop == False:
+        stock = input("Please enter the amount of Stock of the Product: ")
+        int(stock)
+        if type(stock) == int & stock >= 0:
+            loop = True
+        else:
+            print("Invalid amount of Stock, please try again")
+    loop = False
+    while loop == False:
+        month_units = input("Please enter the amount of stock made in a month: ")
+        int(month_units)
+        if type(month_units) == int & month_units >= 0:
+            loop = True
+        else:
+            print("Invalid Monthly Units, please try again")
     App_product = Product(code,name,price,manu_cost,stock,month_units)
